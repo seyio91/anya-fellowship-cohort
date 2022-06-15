7be3b0b9cd35ec5684526bcaf2f7ef09c94122f7
seyio91 <seyi_obaweya@yahoo.com>
2022-06-15 13:05:16 +0200

    Edit VPC name

--- task4/terraform/main.tf
+++ task4/terraform/main.tf
@@ -27,7 +27,7 @@ module "vpc" {
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
   enable_nat_gateway = true
-  enable_vpn_gateway = true
+  enable_vpn_gateway = false
 
   tags = local.tags
 }
